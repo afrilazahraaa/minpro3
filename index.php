d01bd68430264198d8152e0538cc19276300ec49
afrilazahraaa <afrilazahrap@gmail.com>
2024-04-05 00:25:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,534 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" href="responsive.css"/>
+
+    <!-- Aos -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css"/>
+
+    <title>Cookies_Website</title>
+
+    <!-- Favicon -->
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="favicon/site.webmanifest" />
+  </head>
+  <body>
+    <!-- Header -->
+    <header>
+      <div id="header-info">
+        <div class="header-info large-container">
+          <div
+            class="header-info-items"
+            data-aos="fade-in"
+            data-aos-delay="1200"
+          >
+            <!-- Location -->
+            <div>
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              >
+                <path
+                  fill="#FFF"
+                  d="G453+HQH, Lok Bahu, Kec. Sungai Kunjang, Kota Samarinda, Kalimantan Timur 75243"
+                />
+              </svg>
+              <span> 317 Pacific C, Huntington CA 4538 </span>
+            </div>
+
+            <!-- Email -->
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#FFF"
+                  d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"
+                />
+              </svg>
+              <span>hello@mycookies.com</span>
+            </div>
+          </div>
+
+          <div
+            class="header-info-items info-social"
+            data-aos="fade-in"
+            data-aos-delay="1200"
+          >
+            <a href="https://instagram.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#FFF"
+                  d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+                />
+              </svg>
+            </a>
+
+            <a href="https://youtube.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#FFF"
+                  d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
+                />
+              </svg>
+            </a>
+
+            <a href="https://x.com">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#FFF"
+                  d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Logo and navigation for desktop -->
+      <nav id="nav-desktop">
+        <div class="nav large-container">
+          <ul>
+            <li data-aos="fade-down" data-aos-duration="500">
+              <a href="/">Home</a>
+            </li>
+            <li
+              data-aos="fade-down"
+              data-aos-duration="500"
+              data-aos-delay="100"
+            >
+              <a href="#about">About</a>
+            </li>
+            <li
+              data-aos="fade-down"
+              data-aos-duration="500"
+              data-aos-delay="200"
+            >
+              <a href="#menu">Menu</a>
+            </li>
+          </ul>
+
+          <h1 data-aos="fade-down" data-aos-duration="500" data-aos-delay="300">
+            Cookies Rafina
+          </h1>
+
+          <ul>
+            <li
+              data-aos="fade-down"
+              data-aos-duration="500"
+              data-aos-delay="400"
+            >
+              <a href="#order">Order</a>
+            </li>
+            <li
+              data-aos="fade-down"
+              data-aos-duration="500"
+              data-aos-delay="500"
+            >
+              <a href="#location">Location</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <!-- Logo and navigation for mobile -->
+      <nav id="nav-mobile">
+        <div class="nav-mobile">
+          <div class="nav large-container">
+            <h1
+              data-aos="fade-down"
+              data-aos-duration="500"
+              data-aos-delay="200"
+            >
+              Cookies Rafina
+            </h1>
+
+            <div id="burger-navigation">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="nav-mobile-main">
+          <div>
+            <a href="/">Home</a>
+          </div>
+          <div>
+            <a href="#about">About</a>
+          </div>
+          <div>
+            <a href="#menu">Menu</a>
+          </div>
+          <div>
+            <a href="#order">Order</a>
+          </div>
+          <div>
+            <a href="#location">Location</a>
+          </div>
+        </div>
+      </nav>
+
+      <!-- Hero -->
+      <section id="hero">
+        <div
+          class="hero main-container"
+          data-aos="fade-in"
+          data-aos-duration="1000"
+          data-aos-delay="800"
+        >
+          <h4>ENJOY AN UNFORGETTABLE TASTE EXPERIENCE!!</h4>
+          <h1>Cookies Rafina</h1>
+          <div class="hero-cta">
+            <a href="#menu">
+              <button class="cta-button">See Menu</button>
+            </a>
+
+            <a href="#order">
+              <button class="cta-button">Book A Table</button>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <div class="header-shadow"></div>
+    </header>
+    <!-- End header -->
+
+    <!-- About -->
+    <section id="about">
+      <div class="about main-container">
+        <p data-aos="fade-up">
+          selamat datang di website rafina cookies.rafina cookies berdisi sejak tahun 20018 hingga saat ini, 
+          dengan konsisten rasa yang dimiliki oleh rafina cookis dapat bertahan meskipun sempar dilanda covid 19
+        </p>
+        <p data-aos="fade-up">Thank you for creating lasting memory with us.</p>
+      </div>
+    </section>
+
+    <!-- Menu -->
+    <section id="menu">
+      <div class="menu main-container">
+        <div class="menu-title" data-aos="fade-up">
+          <h5>Explore</h5>
+          <h1>Our Menu</h1>
+          <p>
+            silakan melihat farian menu cookies!
+          </p>
+        </div>
+
+        <!-- Makanan utama -->
+        <div class="sub-menu">
+          <div class="sub-section-title" data-aos="fade-up">
+            <h2>Main Course</h2>
+            <p>dibuat dengan bahan premium</p>
+
+            <div class="standing-line"></div>
+          </div>
+
+          <!-- Detail menu dan harga -->
+          <div class="menu-details">
+            <div class="menu-detail" data-aos="fade-up">
+              <div class="menu-item">
+                <div>
+                  <h4>cookies choco</h4>
+                  <p>choco and chip</p>
+                </div>
+                <div>Rp. 35k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies lotus</h4>
+                  <p>slay and biscoof</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies chees</h4>
+                  <p>Pancetta, parmesan cheese, egg</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+            </div>
+
+            <div class="menu-detail" data-aos="fade-up">
+              <div class="menu-item">
+                <div>
+                  <h4>cookies chrispy</h4>
+                  <p>milk and butter</p>
+                </div>
+                <div>Rp. 35k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies abon</h4>
+                  <p>Chicken breast and pesto</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies chezzy milk</h4>
+                  <p>Pancetta, parmesan cheese, egg</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+            </div>
+
+            <div class="menu-detail" data-aos="fade-up">
+              <div class="menu-item">
+                <div>
+                  <h4>cookies lemon</h4>
+                  <p>Lemon and Butter Sauce</p>
+                </div>
+                <div>Rp. 35k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies grep</h4>
+                  <p>anggur sauce</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+
+              <div class="menu-item">
+                <div>
+                  <h4>cookies manggo</h4>
+                  <p>manggo sauce</p>
+                </div>
+                <div>Rp. 40k</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Main course carousel -->
+    <div
+      class="menu-carousel main-course-carousel"
+      data-aos="fade-up"
+      data-aos-duration="500"
+      data-aos-delay="100"
+    >
+      <div class="menu-carousel-slide main-course-carousel-slide">
+        <span class="menu-img">
+          <img src="./images/main-dish/1.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/2.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/3.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/4.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/5.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/6.png" />
+          <span class="tooltiptext">cookiesr</span>
+        </span>
+        <span class="menu-img">
+          <img src="./images/main-dish/7.png" />
+          <span class="tooltiptext">cookies</span>
+        </span>
+      </div>
+    </div>
+
+    <!-- Order -->
+    <section id="order">
+      <div class="order main-container">
+        <div class="menu-title" data-aos="fade-up">
+          <h5></h5>
+          <h1>Order</h1>
+          <p>Pesan Cookies silakan Registrasi/Login terlebih dahulu</p>
+          <div class="login-register">
+            <button class="login-button active">Login</button>
+            <button class="register-button">Register</button>
+          </div>
+        </div>
+        <div class="contact-left">
+                <form 
+                  class="contact-form"
+                  action="https://formspree.io/f/xwkgnapn"
+                  method="POST"
+                >
+                  <div>
+                    <input type="text" placeholder="Name" name="name" />
+                  </div>
+                  <div>
+                    <input type="email" placeholder="Email" name="email" />
+                  </div>
+                  <div>
+                    <textarea
+                      name="message"
+                      id="message"
+                      cols="30"
+                      rows="10"
+                      placeholder="Message"
+                    ></textarea>
+                  </div>
+                  <div>
+                    <button class="btn-submit">Send Message</button>
+                  </div>
+                </form>
+              </div>
+      
+              <div class="contact-right">
+                <div class="contact-item">
+                  <div class="contact-item-icon">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="22"
+                      height="22"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        d="M12 1c-3.148 0-6 2.553-6 5.702 0 3.148 2.602 6.907 6 12.298 3.398-5.391 6-9.15 6-12.298 0-3.149-2.851-5.702-6-5.702zm0 8c-1.105 0-2-.895-2-2s.895-2 2-2 2 .895 2 2-.895 2-2 2zm12 14h-24l4-8h3.135c.385.641.798 1.309 1.232 2h-3.131l-2 4h17.527l-2-4h-3.131c.435-.691.848-1.359 1.232-2h3.136l4 8z"
+                      />
+                    </svg>
+                </div>
+            </div>
+      </div>
+    </section>
+
+    <!-- Contact & location -->
+    <div id="location">
+      <div class="location">
+        <div class="map" data-aos="fade-up">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.09312872685!2d99.16271577525467!3d3.327165652031974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3031613e18790b15%3A0x7e1efd7757c4d15!2sTanah%20lapang%20Merdeka!5e0!3m2!1sen!2sid!4v1703163506806!5m2!1sen!2sid"
+            width="100%"
+            style="border: 0; padding: 0; margin: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+      <div class="footer-info">
+        <div class="footer-info-items main-container">
+          <div data-aos="fade-up">
+            <h3>Welcome to Cookies Rafina</h3>
+            <p>
+              Welcome to Cookies Rafina, where every bite is a story.
+            </p>
+            <p>
+              the warmth of heartfelt hospitality invite you to unwind and
+              savor moments of delightful indulgence.
+            </p>
+          </div>
+
+          <div data-aos="fade-up" data-aos-delay="100">
+            <h3>Visit</h3>
+            <p>317 Pacific C Hwy, Huntington, CA 92648</p>
+
+            <h3>Talk</h3>
+            <p>123.456-7</p>
+
+            <h3>Write</h3>
+            <p>eat@rafinacookies.com</p>
+          </div>
+
+          <div data-aos="fade-up" data-aos-delay="200">
+            <div>
+              <h3>Hours</h3>
+              <div>
+                <p>MONDAY thru FRIDAY</p>
+                <p>11am - 9pm</p>
+              </div>
+
+              <div>
+                <p>SATURDAY and SUNDAY</p>
+                <p>10am - 12pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer-copyright">
+        <div class="footer-items large-container">
+          <div class="footer-left">
+            <!-- &copy; 2024 Soulkitchen. All rights reserved. -->
+          </div>
+
+          <div class="footer-mid">Cookies Rafina</div>
+
+          <div class="footer-right">Taste the difference</div>
+        </div>
+      </div>
+    </footer>
+    <!-- End footer -->
+
+    <!-- Javascript -->
+  </body>
+  <script src="./js/script.js" type="text/javascript"></script>
+
+  <!-- Aos script -->
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init({
+      // once: true, // whether animation should happen only once - while scrolling down
+    });
+  </script>
+</html>
---
+++ js/script.js
@@ -0,0 +1,43 @@
+const form = document.querySelector("#reservation-item-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tableName = document.querySelector("#table_name").value;
+  const tableSize = document.querySelector("#table_capacity").value;
+  const date = document.querySelector("#order_date").value;
+  const order = document.querySelector("#order").value;
+
+  const orderText = `Halo, saya ingin reservasi meja untuk ${tableSize} orang, atas name ${tableName} di tanggal ${date}
+  
+  Order : 
+  ${order}
+  `;
+
+  window.location.replace(`https://wa.me/083826219440?text=${orderText}`);
+});
+
+// Hamburger icon animation
+const hamburger = document.querySelector("#burger-navigation");
+const mobileNavigation = document.querySelector(".nav-mobile-main");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+  mobileNavigation.classList.toggle("menu-active");
+});
+
+// Carousel
+const mainCourseCarousel = document
+  .querySelector(".main-course-carousel-slide")
+  .cloneNode(true);
+document.querySelector(".main-course-carousel").appendChild(mainCourseCarousel);
+
+const drinksCarousel = document
+  .querySelector(".drinks-carousel-slide")
+  .cloneNode(true);
+document.querySelector(".drinks-carousel").appendChild(drinksCarousel);
+
+const snacksCarousel = document
+  .querySelector(".snacks-carousel-slide")
+  .cloneNode(true);
+document.querySelector(".snacks-carousel").appendChild(snacksCarousel);
